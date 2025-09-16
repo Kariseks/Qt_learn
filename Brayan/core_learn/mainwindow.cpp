7e4056f22872d3bbc5354d152d24c615cbf734a7
Kariseks <gasiorkarolpl@gmail.com>
2025-09-16 23:19:33 +0200

    Lambda basics

--- Brayan/core_learn/mainwindow.cpp
+++ Brayan/core_learn/mainwindow.cpp
@@ -6,9 +6,20 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    createLambda();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::createLambda()
+{
+    auto func = []() -> int {return 0;};
+    qDebug() << func() << "I am your father looke \n"<< Qt::endl;
+
+    //lambda funcitons, that accepts params
+    [](int a, int b) {qDebug() << "a+b= " << a+b << Qt::endl; } (1,2);
 }
